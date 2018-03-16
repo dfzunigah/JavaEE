1f771f79d261b9e35c3ef0e896c82f57cfd2a141
Daniel <dfzunigah@unal.edu.co>
2018-03-16 00:00:51 -0500

    Add files via upload

---
+++ TiendaVirtual/TiendaVirtual-ejb/Java/logica/AdministracionOrden.java
@@ -0,0 +1,12 @@
+package logica;
+
+import javax.ejb.Stateful;
+
+/**
+ * @author Daniel Zuñiga (15/04/18) @ UEC
+ **/
+/*Este es como un carrito de modo que sí requiere recordar su estado.*/
+@Stateful
+public class AdministracionOrden implements AdministracionOrdenLocal {
+
+}
---
+++ TiendaVirtual/TiendaVirtual-ejb/Java/logica/AdministracionPersistencia.java
@@ -0,0 +1,14 @@
+package logica;
+
+import javax.ejb.Stateless;
+
+/**
+ * @author Daniel Zuñiga (15/04/18) @ UEC
+ **/
+/*Este lo que hace es comunicarse con la BD.*/
+@Stateless
+public class AdministracionPersistencia implements AdministracionPersistenciaLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
