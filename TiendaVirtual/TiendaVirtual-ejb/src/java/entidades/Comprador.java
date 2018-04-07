ef56cb53dab48e0088f3e8ebb7740c062a8b2cde
Daniel <dfzunigah@unal.edu.co>
2018-04-07 10:25:31 -0500

    Update Comprador.java

--- TiendaVirtual/TiendaVirtual-ejb/src/java/entidades/Comprador.java
+++ TiendaVirtual/TiendaVirtual-ejb/src/java/entidades/Comprador.java
@@ -22,10 +22,6 @@ import javax.xml.bind.annotation.XmlTransient;
 @DiscriminatorValue(value="C")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Comprador extends Persona{
-    /*Siempre que exista una relación bidireccional, siempre hay que establecer
-      quién es el dueño de la relación. Si esa relación es muchos a 1, el dueño de
-      la relación es el lado muchos. Por ejemplo, acá el dueño de la relación es
-      Orden y el atributo que lo relaciona sería "comprador" del tipo Comprador.*/
     @XmlTransient
     @OneToMany(mappedBy="comprador")
     private List<Orden> ordenes;
