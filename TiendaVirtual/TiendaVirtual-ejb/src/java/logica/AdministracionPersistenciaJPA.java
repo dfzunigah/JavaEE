090bf54c49af0c41baa3474b6e14869a655d1687
Daniel <dfzunigah@unal.edu.co>
2018-04-07 07:18:58 -0500

    Update AdministracionPersistenciaJPA.java

--- TiendaVirtual/TiendaVirtual-ejb/src/java/logica/AdministracionPersistenciaJPA.java
+++ TiendaVirtual/TiendaVirtual-ejb/src/java/logica/AdministracionPersistenciaJPA.java
@@ -6,6 +6,8 @@ import entidades.InformaciónEnvio;
 import entidades.InformaciónFactura;
 import entidades.Orden;
 import entidades.Producto;
+import excepciones.CreacionOrdenException;
+import excepciones.ModificacionProductoException;
 import java.util.Date;
 import java.util.List;
 import javax.annotation.Resource;
@@ -14,21 +16,29 @@ import javax.ejb.Stateless;
 import javax.ejb.Timeout;
 import javax.ejb.Timer;
 import javax.ejb.TimerService;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.interceptor.Interceptors;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import notificaciones.NotificacionInterceptor;
 
 /**
- * @author Daniel Zuñiga (22/03/18) @ UEC
+ * @author Daniel Zuñiga (04/04/18) @ UEC
  **/
 @Stateless
+@TransactionManagement(TransactionManagementType.CONTAINER)
+@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
 public class AdministracionPersistenciaJPA implements AdministracionPersistenciaJPALocal {
     /*Esto reemplaza el manejo de conexión de la DB, lo de pre y post.*/
-    @PersistenceContext
+    @PersistenceContext(unitName = "TiendaVirtual-ejbPU")
     private EntityManager em;
     /*Este es modelo programatico. Pero acá no se sigue la POA.*/
-    @Resource
-    private TimerService timerService;
+    //@Resource
+    //private TimerService timerService;
     
     /*Con el EntityManager sencillamente sale todo lo que hicimos ayer sale con una línea.
       El método find sirve para hacer busquedas por llave primaria. Por debajo lo de
@@ -43,30 +53,38 @@ public class AdministracionPersistenciaJPA implements AdministracionPersistencia
     }
 
     @Override
-    public Integer crearOrden(Orden orden) {
-        /*El "persist" sirve para hacer persistencia, para colocar registros o
-          mejor dicho hace un insert.*/
-        em.persist(orden);
-        /*Línea añadida para Timer programatico. El parametro debe ser serializable.
-          Así si yo coloco el Timer */
-        timerService.createTimer(15000, orden);
+    @Interceptors(NotificacionInterceptor.class)
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public Integer crearOrden(Orden orden) throws CreacionOrdenException{
+        try{
+            /*El "persist" sirve para hacer persistencia, para colocar registros o
+              mejor dicho hace un insert.*/
+            em.persist(orden);
+            /*Línea añadida para Timer programatico. El parametro debe ser serializable.
+              Así si yo coloco el Timer */
+            //timerService.createTimer(15000, orden);
+        }catch(Exception ex){
+            throw new CreacionOrdenException();
+        }
         return orden.getId();
     }
     
-    @Timeout
+    /*@Timeout
     private void timerCrearOrden(Timer timer){
         Orden orden = (Orden) timer.getInfo();
         System.out.println("Se ha enviado la orden a la dirección " +
                             orden.getInformaciónEnvio().getDireccion());
-    }
+    }*/
 
     @Override
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
     public Integer crearInformacionEnvio(InformaciónEnvio ie) {
         em.persist(ie);
         return ie.getId();
     }
 
     @Override
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
     public Integer crearInformacionFactura(InformaciónFactura infFac) {
         em.persist(infFac);
         return infFac.getId();
@@ -74,11 +92,20 @@ public class AdministracionPersistenciaJPA implements AdministracionPersistencia
 
     /*El método "merge" permite hacer update sobre un campo.*/
     @Override
-    public void modificarProductos(List<Producto> productos, Orden orden) {
-        for (int i = 0; i < productos.size(); i++){
-            Producto producto = productos.get(i);
-            producto.setOrden(orden);
-            em.merge(producto);
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    public void modificarProductos(List<Producto> productos, Orden orden) throws ModificacionProductoException{
+        try{
+            for (int i = 0; i < productos.size(); i++){
+                Producto producto = productos.get(i);
+                producto.setOrden(orden);
+                em.merge(producto);
+            }
+            /*Esta línea es para probar si funciona el rollback, sencillamente
+              lanza un error. No la comente si quiere comprobar el rollback,
+              comentala si quiere probar el commit.*/
+            //throw new ModificacionProductoException();
+        }catch(Exception ex){
+            throw new ModificacionProductoException();
         }
     }
 
@@ -95,6 +122,7 @@ public class AdministracionPersistenciaJPA implements AdministracionPersistencia
     }
 
     @Override
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
     public Integer crearBitacora(Bitacora bitacora) {
         em.persist(bitacora);
         return bitacora.getId();
@@ -107,7 +135,7 @@ public class AdministracionPersistenciaJPA implements AdministracionPersistencia
         return compradores;
     }
     
-    @Schedule(second = "*/20", minute="*", hour="*")
+    //@Schedule(second = "*/20", minute="*", hour="*")
     private void timerHora(){
         System.out.println("Hora TiendaVirtual: " + new Date());
     }
